8f543aae5fea9b6429a748e25a050e467b16da11
rangertaha <rangertaha@gmail.com>
2014-04-07 09:01:40 -0400

    update

---
+++ test/send.js
@@ -0,0 +1,15 @@
+
+//var event = require('../lib/saltevents');
+var assert = require('assert');
+
+
+
+
+
+describe('user',function(){
+    describe('create',function(){
+        console.log("Hello Test")
+    });
+});
+
+
